6f74422d6960921e92d9a73c2e49e519ed75c4bc
Jethro Coma <jethrocoma@gmail.com>
2024-10-09 15:00:53 -0700

    refactor api.tsx to create Requests methods

--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,5 @@
-import { Dog } from "./types";
-export const baseUrl = "http://localhost:3000";
+import { Dog } from './types';
+export const baseUrl = 'http://localhost:3000';
 
 const getAllDogs = () => {
   // fill out method
@@ -11,15 +11,47 @@ const getAllDogs = () => {
   });
 };
 
-const postDog = (dog: Omit<Dog, "id">) => {
+const postDog = (dog: Omit<Dog, 'id'>) => {
   // fill out method
+  return fetch(`${baseUrl}/dogs`, {
+    body: JSON.stringify(dog),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP request failed with status ${res.status}!`);
+    }
+    return res.json();
+  });
 };
-const deleteDogRequest = () => {
+const deleteDogRequest = (dog: Dog) => {
   // fill out method
+  return fetch(`${baseUrl}/dogs/${dog.id}`, {
+    method: 'DELETE',
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP request failed with status ${res.status}!`);
+    }
+    return res.json();
+  });
 };
 
-const patchFavoriteForDog = () => {
+const patchFavoriteForDog = (dog: Pick<Dog, 'id' | 'isFavorite'>) => {
   // fill out method
+  return fetch(`${baseUrl}/dogs/${dog.id}`, {
+    body: JSON.stringify(dog),
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP request failed with status ${res.status}!`);
+    }
+    return res.json();
+  });
 };
 
 export const Requests = {
