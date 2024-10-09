b829387d1617fc2902af8ae5b52790c5cd56fd2b
Jethro Coma <jethrocoma@Jethros-MacBook-Pro.local>
2024-10-09 14:20:24 -0700

    make change to testBranch

--- src/api.tsx
+++ src/api.tsx
@@ -1,8 +1,17 @@
+import { Dog } from "./types";
+export const baseUrl = "http://localhost:3000";
+
 const getAllDogs = () => {
   // fill out method
+  return fetch(`${baseUrl}/dogs`).then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP request failed with status ${res.status}`);
+    }
+    return res.json();
+  });
 };
 
-const postDog = () => {
+const postDog = (dog: Omit<Dog, "id">) => {
   // fill out method
 };
 const deleteDogRequest = () => {
