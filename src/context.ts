f627ccc1129957ff75611ad8559501edaaf4c54e
Jethro Coma <jethrocoma@Jethros-MacBook-Pro.local>
2024-10-19 11:54:29 -0700

    optimistic branch fist commit.

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,13 @@
-import { createContext, useContext } from 'react'
-import { DogContextType } from './types'
-export const DogContext = createContext<DogContextType | undefined>(undefined)
+import { createContext, useContext } from "react";
+import { DogContextType } from "./types";
+export const DogContext = createContext<DogContextType | undefined>(undefined);
 
 export const useDogContext = () => {
-  const context = useContext(DogContext); 
+  const context = useContext(DogContext);
   if (!context) {
-    throw new Error('useDogContext must be used within a DogContextProvider')
+    throw new Error("useDogContext must be used within a DogContextProvider");
   }
-  return context; 
-}
+  return context;
+};
+
+//this should not be seen in other branches.s
