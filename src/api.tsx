60aded76168d431d3d6193d49acd002b43d53370
Jethro Coma <jethrocoma@gmail.com>
2025-04-18 09:55:35 -0700

    Fix: change baseurl in api.ts to empty string

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,8 @@
 import { Dog } from "./types";
 import { toast } from "react-hot-toast";
 
-export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "http://localhost:3000";
+export const baseUrl = "";
 
 const getAllDogs = () => {
   return fetch(`${baseUrl}/dogs`).then((res) => {
